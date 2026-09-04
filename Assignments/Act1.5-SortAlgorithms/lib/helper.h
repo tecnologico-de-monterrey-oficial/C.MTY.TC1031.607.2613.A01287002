11f38a671b448527086d16982668341505824e12
Andrés Rodríguez Cantú <a01287002@tec.mx>
2026-09-04 16:36:43 -0600

    Added merge sort (#13)

--- Assignments/Act1.5-SortAlgorithms/lib/helper.h
+++ Assignments/Act1.5-SortAlgorithms/lib/helper.h
@@ -3,6 +3,8 @@
 #include <vector>
 #include <utility>
 
+// --- Quicksort ---
+
 template <typename T>
 int partition(std::vector<T>& arr, int low, int high) {
     T pivot = arr[high];
@@ -26,4 +28,49 @@ void quickSort(std::vector<T>& arr, int low=0, int high=-1) {
 
     quickSort(arr, low, par - 1);
     quickSort(arr, par + 1, high); 
+}
+
+// --- mergesort ---
+
+template <typename T>
+void merge(std::vector<T>& arr, int low, int mid, int high) {
+    std::vector<T> tmp;
+    tmp.reserve(high-low+1);
+
+    for(int i=low; i<= mid; i++) {
+        tmp.push_back(arr[i]);
+    }
+    for(int i=mid+1; i<= high; i++) {
+        tmp.push_back(arr[i]);
+    }
+
+    int i = 0, j = mid - low + 1;
+    for(int k=low; k<= high; k++) {
+        if(i < mid - low + 1 && j < high - low + 1) {
+            if(tmp[i] < tmp[j]) {
+                arr[k] = tmp[i];
+                i++;
+            } else {
+                arr[k] = tmp[j];
+                j++;
+            }
+        } else if(i < mid - low + 1) {
+            arr[k] = tmp[i];
+            i++;
+        } else {
+            arr[k] = tmp[j];
+            j++;
+        }
+    }
+}
+
+template <typename T>
+void mergeSort(std::vector<T>& arr, int low, int high) {
+    if (low >= high) return;
+    int mid = low + (high - low) / 2;
+
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
 }
--- Assignments/Act1.5-SortAlgorithms/main.cpp
+++ Assignments/Act1.5-SortAlgorithms/main.cpp
@@ -11,6 +11,13 @@ int main() {
     for(int i = 0; i < n; i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
-    
+
+    std::vector<int> arr2 = {12, 11, 13, 5, 6, 7};
+    mergeSort(arr2, 0, arr2.size() - 1);
+    std::cout << "Sorted array mergesort: ";
+    for(int i = 0; i < arr2.size(); i++)
+        std::cout << arr2[i] << " ";
+    std::cout << std::endl;
+
     return 0;
 }
